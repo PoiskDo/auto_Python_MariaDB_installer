13f99056fc0ea84e3fceb44420f17b65f1ce5e88
dorincop <101493930+dorincop@users.noreply.github.com>
2022-07-14 03:18:52 +0900

    Update python_MariaDB_installer.sh

--- python_MariaDB_installer.sh
+++ python_MariaDB_installer.sh
@@ -1,1 +1,7 @@
-
+echo "[ 파이썬+마리아DB 설치기 by dorincop ]"
+echo "[ 파이썬, 마리아DB를 설치중... ]"
+echo "[ 예상 소요시간 10-20분... ]"
+sudo apt-get update
+sudo apt-get install python3
+sudo apt install mariadb-server
+sudo apt install mariadb-plugin-mroonga
